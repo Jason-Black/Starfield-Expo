c970cbe679d9cc0dd73c2552414df04f377ce8a3
Jason-Black <71369201+Jason-Black@users.noreply.github.com>
2024-07-21 19:08:02 +0000

    try to get tailwinds to load

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['/workspaces/Starfield-Expo/index.html', '/workspaces/src/**/*.{js,css}'],
+  content: ['/workspaces/Starfield-Expo/index.html', '/workspaces/src/**/*.{js,css}', '/workspaces/Starfield-Expo/main.js' ],
   theme: {
     extend: {},
   },
